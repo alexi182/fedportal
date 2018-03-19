a1f1b53492f7341a5f83c206c929be3c72a2885f
alex182 <format255@bk.ru>
2018-03-19 16:34:26 +0300

    map

---
+++ js/06-vectop-map.js
@@ -0,0 +1,46 @@
+
+$('#map').vectorMap({
+      map: 'ru_reg_lcc_ru',
+        
+    markerStyle: {
+        initial: {
+        fill: '#F8E23B',
+        stroke: '#383f47'
+      }
+    },
+     backgroundColor: '#f1f4f7',
+    //markers: [
+    //  {latLng: [246.90, 338.45], name: "Italy"},
+    //  {latLng: [326.02, 350.55], name: 'Bahrain'},
+    //],
+
+      zoomButtons : true,
+      zoomMax: 4,
+      regionsSelectable: true,
+      regionsSelectableOne: true,
+      hoverColor: '#4d76a8',
+      regionStyle: {
+         initial: {
+            fill: '#add7f7',
+            "fill-opacity": 1,
+            stroke: '#fff',
+            "stroke-width": 0.2,
+            "stroke-opacity": 0.5,
+            "scale": ['#C8EEFF', '#0071A4']
+         },
+         hover: {
+            fill: '#4d76a8',
+            cursor: 'pointer'
+         },
+         selected: {
+            fill: '#4d76a8'
+         },
+         selectedHover: {
+             fill: '#4d76a8'
+         }
+      },
+      onRegionClick: function (e, code) {
+         // code это "Идентификатор Open Street Maps"
+         //console.log(code);
+      }
+});
